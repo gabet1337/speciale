4d15a15998e4c9c1ecc475e8b93c962ff1d1c97d
Peter Gabrielsen <peterg@cs.au.dk>
2016-02-08 07:24:21 -0800

    buggy sync procedure

--- src/stream/stream.hpp
+++ src/stream/stream.hpp
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <error.h>
+#include <algorithm>
+#include <iostream>
 
 namespace io {
   template <typename T>
@@ -24,16 +26,17 @@ namespace io {
     size_t size();
     bool empty();
     bool eof();
-    void sync();
   private:
     int file_descriptor;
     void fill();
+    void sync();
+    off_t seek(size_t offset, int whence);
     size_t buffer_size, file_pos, buffer_pos;
     off64_t file_size;
+    off_t buffer_start;
     T* buffer;
     std::string file_name;
-    bool is_open;
-    bool b_eof;
+    bool is_open, b_eof, is_dirty;
   };
 
   template <typename T>
@@ -51,17 +54,19 @@ namespace io {
 
   template <typename T>
   void buffered_stream<T>::open(std::string file_name) {
-    file_descriptor = ::open(file_name.c_str(), O_RDWR | O_CREAT | O_LARGEFILE);
-    //todo write file name
+    file_descriptor = ::open(file_name.c_str(), O_RDWR | O_CREAT | O_LARGEFILE, S_IRWXU);
     if (file_descriptor == -1) { perror(std::string("Error opening file '").append(file_name).append("'").c_str()); exit(errno); }
     is_open = true;
     b_eof = false;
+    is_dirty = false;
     this->file_name = file_name;
     file_pos = 0;
-    buffer_pos = buffer_size/sizeof(T);
+    buffer_pos = 0;
+    buffer_start = 0;
     struct stat sb;
     fstat(file_descriptor, &sb);
     file_size = sb.st_size;
+    fill();
   }
 
   template <typename T>
@@ -69,6 +74,7 @@ namespace io {
     if (!is_open) {
       error(1, ENOENT, "Error closing file");
     }
+    sync();
     if (::close(file_descriptor) == -1) {
       perror(std::string("Error closing file '").append(file_name).append("'").c_str());
       exit(errno);
@@ -79,20 +85,37 @@ namespace io {
   template <typename T>
   bool buffered_stream<T>::eof() {
     if (b_eof) return true;
-    return file_pos >= file_size;
+    return buffer_start >= file_size;
   }
 
   template <typename T>
   void buffered_stream<T>::fill() {
-    if (eof()) return;
-    ssize_t bytes_read = ::read(file_descriptor, buffer, buffer_size);
-    if (bytes_read == -1) {
-      perror(std::string("Error reading from file '").append(file_name).append("'").c_str());
-      exit(errno);
+    std::cout << "eof at this point: " << eof() << std::endl;
+    //std::cout << "file_pos: " << file_pos << std::endl;
+    //std::cout << "buffer_pos: " << buffer_pos << std::endl;
+    //std::cout << "file_size: " << file_size << std::endl;
+    if (is_dirty) sync();
+    if (!eof()) {
+      buffer_start = lseek(file_descriptor,0,SEEK_CUR);
+      //std::cout << "buffer_start: " << buffer_start << std::endl;
+      if (buffer_start == -1) {
+        perror(std::string("Error seeking file: ").append(file_name).append("'").c_str());
+        exit(errno);
+      }
+      ssize_t bytes_read = ::read(file_descriptor, buffer, buffer_size);
+      if (bytes_read == -1) {
+        perror(std::string("Error reading from file '").append(file_name).append("'").c_str());
+        exit(errno);
+      }
+      if (bytes_read < buffer_size) b_eof = true;
+      file_pos += bytes_read;
+      std::cout << "trolololo" << std::endl;
+    } else {
+      file_pos = seek(0,SEEK_END);
+      buffer_start = file_pos;
+      std::cout << "file_pos at this point: " << file_pos << std::endl;
     }
-    if (bytes_read < buffer_size) b_eof = true;
-    file_pos += bytes_read;
-    buffer_pos = 0;
+    buffer_pos = 0; 
   }
 
   template <typename T>
@@ -104,7 +127,47 @@ namespace io {
 
   template <typename T>
   void buffered_stream<T>::write(T item) {
-    
+    is_dirty = true;
+    if (buffer_pos >= buffer_size/sizeof(T)) fill();
+    buffer[buffer_pos++] = item;
+  }
+
+  template <typename T>
+  void buffered_stream<T>::sync() {
+    std::cout << "eof: " << eof() << std::endl;
+    std::cout << "file_pos: " << file_pos << std::endl;
+    std::cout << "buffer_pos: " << buffer_pos << std::endl;
+    std::cout << "file_size: " << file_size << std::endl;
+
+    off_t cur_pos = seek(0, SEEK_CUR);
+    //std::cout << "current_pos: " << cur_pos << std::endl;
+    std::cout << "buffer_start: " << buffer_start << std::endl;
+    seek(buffer_start, SEEK_SET);
+    size_t bytes_written = ::write(file_descriptor, buffer, buffer_pos*sizeof(T));
+    if (bytes_written == -1) {
+      perror(std::string("Error on syncing buffer for file: '").append(file_name).append("'").c_str());
+      exit(errno);
+    }
+    file_size = std::max((size_t) file_size, buffer_start+bytes_written);
+    std::cout << "file_size at this point: " << file_size << std::endl;
+    std::cout << seek(cur_pos,SEEK_SET) << std::endl;
+    std::cout << "bytes_written: " << bytes_written << std::endl;
+    is_dirty = false;
+  }
+
+  template <typename T>
+  off_t buffered_stream<T>::seek(size_t offset, int whence) {
+    off_t pos = lseek(file_descriptor,offset,whence);
+    if (pos == -1) {
+      perror(std::string("Error seeking file: ").append(file_name).append("'").c_str());
+      exit(errno);
+    }
+    return pos;
+  }
+  
+  template <typename T>
+  size_t buffered_stream<T>::size() {
+    return (size_t) file_size;
   }
 
 };
--- src/stream/test_stream.cpp
+++ src/stream/test_stream.cpp
@@ -7,22 +7,31 @@
 using namespace std;
 
 void test_open_close() {
+
+  cout << "starting test_open_close" << endl;
+  
   io::buffered_stream<char> bs(1);
   bs.open("stream/testfiles/test1.txt");
   bs.close();
 }
 
 void test_read() {
+
+  cout << "starting test_read" << endl;
+  
   io::buffered_stream<char> bs(1);
   bs.open("stream/testfiles/test1.txt");
-
   string true_string = "Hell";
   string actual_string;
   for (int i = 0; i < 4; i++) actual_string += bs.read();
+  bs.close();
   assert( (true_string == actual_string) && "Stream didn't read correct");
 }
 
 void test_write() {
+
+  cout << "starting test_write" << endl;
+  
   io::buffered_stream<char> bs(1);
   bs.open("stream/testfiles/test_out.txt");
   string true_string = "Hello World!";
@@ -32,20 +41,78 @@ void test_write() {
   ifstream file;
   file.open("stream/testfiles/test_out.txt");
   string actual_string;
-  while (file >> actual_string);
+  getline(file, actual_string);
+  file.close();
+  assert( (actual_string == true_string) && "Stream didn't write correctly");
+
+}
+
+void test_interleave_read_write() {
+
+  cout << "starting test_read_write" << endl;
+  
+  io::buffered_stream<char> bs(2);
+  bs.open("stream/testfiles/test_interleaved.txt");
+  string true_string = "Hello World";
+  for (size_t i = 0; i < true_string.size(); i++)
+    if (i % 2)
+      assert ( (bs.read() == true_string[i]) && "Stream didn't read correctly");
+    else bs.write(true_string[i]);
+  bs.close();
+  
+  ifstream file;
+  file.open("stream/testfiles/test_interleaved.txt");
+  string actual_string;
+  getline(file, actual_string);
   file.close();
   assert( (actual_string == true_string) && "Stream didn't write correctly");
 
 }
 
 void test_read_point() {
+
+  cout << "starting test_read_point" << endl;
+
+  system("rm stream/testfiles/test_points.txt");
+  
   io::buffered_stream<point> bs(1);
-  // bs.open(
+  bs.open("stream/testfiles/test_points.txt");
+  bs.write(point(1,2));
+  bs.write(point(3,2));
+  bs.close();
+  cout << "din mor" << endl;
+  bs.open("stream/testfiles/test_points.txt");
+  assert ( (point(1,2) == bs.read() && point(3,2) == bs.read()) && "Not the correct point");
+  bs.close();
+}
+
+void test_file_size() {
+
+  cout << "starting test_file_size" << endl;
+
+  system("rm stream/testfiles/test_size.txt");
+  
+  io::buffered_stream<point> bs(1);
+  bs.open("stream/testfiles/test_size.txt");
+  bs.write(point(1,2));
+  bs.write(point(3,2));
+  bs.close();
+
+  bs.open("stream/testfiles/test_size.txt");
+  cout << bs.size() << endl;
+  assert ( (bs.size() == 16) && "Not the right size");
+  bs.close();
 }
 
 int main() {
+  
   test_open_close();
   test_read();
+  test_write();
+  test_interleave_read_write();
+  test_read_point();
+  test_file_size();
+
   cout << "\x1b[32mALL TESTS WERE SUCCESSFUL!\x1b[0m" << endl;
 
   return 0;
