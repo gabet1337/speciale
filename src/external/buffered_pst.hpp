d0090b2dde5837555acad03af69c16cf2741836a
Peter Gabrielsen <peterg@cs.au.dk>
2016-02-19 05:37:22 -0800

    up to handle insert buffer overflow

--- src/external/buffered_pst.hpp
+++ src/external/buffered_pst.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <algorithm>
+#include <iterator>
 
 namespace ext {
 
@@ -30,6 +32,13 @@ namespace ext {
       bool is_valid();
 #endif
     private:
+      bool insert_buffer_overflow();
+      bool delete_buffer_overflow();
+      bool point_buffer_overflow();
+      bool point_buffer_underflow();
+      void handle_insert_buffer_overflow();
+      bool is_leaf();
+      bool is_root();
       int id;
       size_t buffer_size;
       std::set<point> insert_buffer, delete_buffer, point_buffer;
@@ -54,20 +63,131 @@ namespace ext {
   void buffered_pst::buffered_pst_node::insert(const point &p) {
     DEBUG_MSG("Inserting point " << p << " into node " << id);
 
-    if (id == 0) {
+    if (is_root()) {
       /* Inserting into root
 	 1.) Remove duplicates of p from Pr, Ir, Dr
 	 2.) If p.y < smallest y in Pr then p is inserted in Ir or Dr, o.w. in Pr.
-	 3.) Of pr overflows then move smallest y-value point to Ir */
-      
+	 3.) If pr overflows then move smallest y-value point to Ir */
+      DEBUG_MSG("remove duplicates of p from Pr, Ir, Dr");
+      point_buffer.erase(p);
+      insert_buffer.erase(p);
+      delete_buffer.erase(p);
+      DEBUG_MSG("Check if put into Ir or Pr");
+      point min_y = *std::min_element(point_buffer.begin(), point_buffer.end(),
+                                      [] (const point &p1, const point &p2) {
+                                        return p1.y < p2.y;
+                                      });
+      if (p.y < min_y.y) insert_buffer.insert(p);
+      else point_buffer.insert(p);
+      if ( point_buffer_overflow() ) {
+        DEBUG_MSG("Pr overflow: Moving point with smallest y-value from Pr to Ir");
+        point_buffer.erase(min_y);
+        insert_buffer.insert(min_y);
+        //TODO: handle insert_buffer overflow here
+        if ( insert_buffer_overflow() ) handle_insert_buffer_overflow();
+      }
     }
+  }
+
+  bool buffered_pst::buffered_pst_node::point_buffer_overflow() {
+    if ( is_root() ) return point_buffer.size() > buffer_size;
+    if ( is_leaf() ) return point_buffer.size() >= buffer_size/2;
+    return point_buffer.size() > buffer_size;
+  }
+
+  bool buffered_pst::buffered_pst_node::point_buffer_underflow() {
+    if ( is_leaf() ) return false;
+    return point_buffer.size() < buffer_size/2;
+  }
+
+  bool buffered_pst::buffered_pst_node::insert_buffer_overflow() {
+    return insert_buffer.size() > buffer_size;
+  }
+  
+  bool buffered_pst::buffered_pst_node::delete_buffer_overflow() {
+    return delete_buffer.size() > buffer_size/4;
+  }
+
+  bool buffered_pst::buffered_pst_node::is_leaf() {
+    return true;
+  }
+
+  bool buffered_pst::buffered_pst_node::is_root() {
+    return id == 0;
+  }
+
+  void buffered_pst::buffered_pst_node::handle_insert_buffer_overflow() {
+    DEBUG_MSG("Starting to handle insert buffer overflow");
   }
 
 #ifdef DEBUG
   bool buffered_pst::buffered_pst_node::is_valid() {
     // TODO check all invariants. Recurse on children.
-    if (insert_buffer.size() > buffer_size) {
+    if ( insert_buffer_overflow() ) {
       DEBUG_MSG("Insert buffer overflow in node " << id);
+      return false;
+    }
+    if ( delete_buffer_overflow() ) {
+      DEBUG_MSG("Delete buffer overflow in node " << id);
+      return false;
+    }
+    if ( point_buffer_overflow() ) {
+      DEBUG_MSG("Point buffer overflow in node " << id);
+      return false;
+    }
+    if ( point_buffer_underflow() ) {
+      DEBUG_MSG("Point buffer underflow in node " << id);
+      return false;
+    }
+
+
+    // no duplicates in Pv, Iv, Dv
+    std::vector<point> intersection;
+    //insert \cap delete = OE
+    std::set_intersection(insert_buffer.begin(), insert_buffer.end(),
+                          delete_buffer.begin(), delete_buffer.end(),
+                          std::back_inserter(intersection));
+    if (!intersection.empty()) {
+      DEBUG_MSG("Duplicate in insert buffer and delete buffer");
+      for (point p : intersection)
+        DEBUG_MSG(p);
+      return false;
+    }
+    intersection.clear();
+    //insert \cap point = OE
+    std::set_intersection(insert_buffer.begin(), insert_buffer.end(),
+                          point_buffer.begin(), point_buffer.end(),
+                          std::back_inserter(intersection));
+    if (!intersection.empty()) {
+      DEBUG_MSG("Duplicate in insert buffer and point buffer");
+      for (point p : intersection)
+        DEBUG_MSG(p);
+      return false;
+    }
+    intersection.clear();
+    // delete \cap point = OE
+    std::set_intersection(delete_buffer.begin(), delete_buffer.end(),
+                          point_buffer.begin(), point_buffer.end(),
+                          std::back_inserter(intersection));
+    if (!intersection.empty()) {
+      DEBUG_MSG("Duplicate in insert buffer and point buffer");
+      for (point p : intersection)
+        DEBUG_MSG(p);
+      return false;
+    }
+
+    // Iv U Dv have y-value less than the points in Pv
+    std::vector<point> union_iv_dv;
+    std::set_union(insert_buffer.begin(), insert_buffer.end(),
+                   delete_buffer.begin(), delete_buffer.end(),
+                   std::back_inserter(union_iv_dv));
+    int min_y = std::min_element(point_buffer.begin(), point_buffer.end(),
+                                 [] (point p1, point p2) { return p1.y < p2.y; })->y;
+    if (std::any_of(union_iv_dv.begin(), union_iv_dv.end(),
+                    [&min_y] (point p) {
+                      return p.y > min_y;
+                    })) {
+      DEBUG_MSG("A point in Iv or Dv has larger y value than a point in Pv");
       return false;
     }
 
--- src/external/test_buffered_pst.cpp
+++ src/external/test_buffered_pst.cpp
@@ -41,6 +41,71 @@ void test_insert() {
   print_success();
 }
 
+void test_buffer_points_less_than_point_buffer_points() {
+  print_description("starting test of invariant testing for all p in Iv U Dv : p < min-y in Pv  ");
+  set<point> insert_buffer, delete_buffer, point_buffer;
+  insert_buffer.insert(point(1,1));
+  delete_buffer.insert(point(1,2));
+  point_buffer.insert(point(1,3));
+  std::vector<point> union_iv_dv;
+  std::set_union(insert_buffer.begin(), insert_buffer.end(),
+                 delete_buffer.begin(), delete_buffer.end(),
+                 std::back_inserter(union_iv_dv));
+  int min_y = std::min_element(point_buffer.begin(), point_buffer.end(),
+                               [] (point p1, point p2) { return p1.y < p2.y; })->y;
+  assert ( (std::any_of(union_iv_dv.begin(), union_iv_dv.end(),
+                        [&min_y] (point p) {
+                          return p.y > min_y;
+                        }) == false));
+
+  point_buffer.insert(point(0,0));
+  min_y = std::min_element(point_buffer.begin(), point_buffer.end(),
+                           [] (point p1, point p2) { return p1.y < p2.y; })->y;
+
+  assert ( (std::any_of(union_iv_dv.begin(), union_iv_dv.end(),
+                        [&min_y] (point p) {
+                          return p.y > min_y;
+                        }) == true));
+
+  print_success();
+}
+
+void test_no_duplicates_in_pv_iv_dv() {
+  print_description("starting test of invariant testing no duplicates in iv,dv,pv");
+
+  std::vector<point> intersection;
+  //insert \cap delete = OE
+  std::set<point> insert_buffer, delete_buffer;
+  insert_buffer.insert(point(1,1)); insert_buffer.insert(point(1,2));
+  delete_buffer.insert(point(2,2)); delete_buffer.insert(point(2,3));
+  std::set_intersection(insert_buffer.begin(), insert_buffer.end(),
+                        delete_buffer.begin(), delete_buffer.end(),
+                        std::back_inserter(intersection));
+  assert( (intersection.empty() == true) );
+
+  insert_buffer.insert(point(2,2));
+  intersection.clear();
+  std::set_intersection(insert_buffer.begin(), insert_buffer.end(),
+                        delete_buffer.begin(), delete_buffer.end(),
+                        std::back_inserter(intersection));
+
+  assert( intersection.empty() == false);
+
+  print_success();
+}
+
+void test_insert_buffer_overflow() {
+  print_description("starting test of insert buffer overflow");
+
+  ext::buffered_pst epst(9,0.5);
+  for (int i = 0; i < 10; i++) epst.insert(point(i,i));
+  assert ( epst.is_valid() );
+  for (int i = 10; i < 18; i++) epst.insert(point(i,i));
+  assert ( epst.is_valid() );
+  epst.insert(point(19,19));
+  print_success();
+}
+
 int main() {
 
   cout << "\033[0;33m\e[4mSTARTING TEST OF CHILD STRUCTURE\e[24m\033[0m" << endl;
@@ -48,6 +113,9 @@ int main() {
   test_test();
   test_construction();
   test_insert();
+  test_buffer_points_less_than_point_buffer_points();
+  test_no_duplicates_in_pv_iv_dv();
+  test_insert_buffer_overflow();
   
   cout << "\x1b[32mALL TESTS WERE SUCCESSFUL!\x1b[0m" << endl;
   return 0;
