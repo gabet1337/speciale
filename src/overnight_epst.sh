1de030873608e64d3bccc80a9cb926d556ecc5bb
Peter Gabrielsen <peterg@cs.au.dk>
2016-03-03 09:19:33 -0800

    Added overnight shell scripts

---
+++ src/overnight_epst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf out.out
+c=1;
+while true
+do
+    echo $c;
+    c=$((c+1));
+    ./epst > out.out;
+    rc=$?;
+    if [[ $rc != 0 ]];
+    then exit $rc; fi;
+    rm out.out;
+done
