f8171bf860f54230d1cc23ba0a88a5ae3309bd18
Peter Gabrielsen <peterg@cs.au.dk>
2016-03-18 19:57:04 +0100

    Added more operators on point

--- src/common/point.hpp
+++ src/common/point.hpp
@@ -16,6 +16,9 @@ public:
   bool operator==(const point& p) {
     return x == p.x && y == p.y;
   }
+  bool operator !=(const point &p) {
+    return !(p.x == x && y == p.y);
+  }
   friend std::ostream& operator<<(std::ostream& os, const point& p) {
     os << "(" << p.x << "," << p.y << ")";
     return os;
@@ -29,6 +32,10 @@ bool operator<(const point &p1, const point &p2) {
 
 bool operator==(const point &p1, const point &p2) {
   return p1.x == p2.x && p1.y == p2.y;
+}
+
+bool operator!=(const point &p1, const point &p2) {
+  return !(p1 == p2);
 }
 
 bool operator<=(const point &p1, const point &p2) {
