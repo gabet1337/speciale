58147b96e437e4d8c9748d33960f9fe8ab2ce194
Peter Gabrielsen <peterg@cs.au.dk>
2016-04-12 02:24:47 -0700

    Outcommented unnescessary tests

--- src/internal/test_boost_r_tree.cpp
+++ src/internal/test_boost_r_tree.cpp
@@ -139,9 +139,9 @@ void test_report_random_1gb(size_t buffer_size, double epsilon) {
 
 int main() {
   cout << "\033[0;33m\e[4mSTARTING TEST OF BOOST R-TREE\e[24m\033[0m" << endl;
-  test_insert();
-  test_report();
-  test_remove_report();
+  // test_insert();
+  // test_report();
+  // test_remove_report();
 
   test_report_random_1gb(4096, 0);
   cout << "\x1b[32mALL TESTS WERE SUCCESSFUL!\x1b[0m" << endl;
