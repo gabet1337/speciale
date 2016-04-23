9d0abcf7a8e50f85828a8c6537122ef20a0a6979
overnight <peterg@cs.au.dk>
2016-04-23 11:11:04 +0200

    input output results on idefix

---
+++ src/experiments/stream_input_speed_experiment_results/2016-04-23.11_05_51/plot_pfs.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/gnuplot
+set terminal postscript eps enhanced color font 'Verdana,9'
+set output 'stream_input_speed_experiment_results/2016-04-23.11_05_51/pfs.eps'
+set style line 11 lc rgb '#808080' lt 1
+set border 3 back ls 11
+set tics nomirror
+set style line 12 lc rgb '#808080' lt 0 lw 1
+set grid back ls 12
+set style line 1 lc rgb '#8b1a0e' pt 1 ps 1 lt 1 lw 2 # --- red
+set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2 # --- green
+set style line 3 lc rgb '#88419d' pt 2 ps 1 lt 1 lw 2 # --- purple
+set style line 4 lc rgb '#225ea8' pt 3 ps 1 lt 1 lw 2 # --- blue
+set style line 5 lc rgb '#000000' pt 4 ps 1 lt 1 lw 2 # --- black
+set style line 6 lc rgb '#00ced1' pt 5 ps 1 lt 1 lw 2 # --- darkturquoise
+set style line 7 lc rgb '#ff00ff' pt 7 ps 1 lt 1 lw 2 # --- magenta
+set style line 8 lc rgb '#87ceeb' pt 8 ps 1 lt 1 lw 2 # --- skyblue
+set key top left
+set xlabel 'N (input size in Mb)'
+set ylabel 'page faults'
+#set xrange [0:0]
+#set yrange [0:0]
+plot 'stream_input_speed_experiment_results/2016-04-23.11_05_51/mmap_mmap' u 1:4 t 'mmap' w lp ls 7, \
+'stream_input_speed_experiment_results/2016-04-23.11_05_51/buffered_stream_buffered' u 1:4 t 'buffered' w lp ls 8, \
+'stream_input_speed_experiment_results/2016-04-23.11_05_51/file_stream_file' u 1:4 t 'file' w lp ls 9
