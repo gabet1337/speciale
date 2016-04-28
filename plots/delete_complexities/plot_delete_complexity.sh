f584cb84467647fde80af3f80f20183cd15a6108
Peter Gabrielsen <peterg@cs.au.dk>
2016-04-28 15:03:46 +0200

    Added theoretical bounds for insert, delete, query

--- plots/delete_complexities/plot_delete_complexity.sh
+++ plots/delete_complexities/plot_delete_complexity.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/gnuplot
 set terminal postscript eps enhanced color font 'Verdana,11'
-set output 'gerth_buffer_size.eps'
+set output 'delete_complexity.eps'
 set style line 11 lc rgb '#808080' lt 1
 set border 3 back ls 11
 set tics nomirror
@@ -11,7 +11,7 @@ set style line 2 lc rgb '#5e9c36' pi 5 pt 6 ps 1 lt 1 lw 2 # --- green
 set style line 3 lc rgb '#88419d' pi 5 pt 2 ps 1 lt 1 lw 2 # --- purple
 set style line 4 lc rgb '#225ea8' pi 5 pt 3 ps 1 lt 1 lw 2 # --- blue
 set style line 5 lc rgb '#000000' pi 5 pt 4 ps 1 lt 1 lw 2 # --- black
-set style line 6 lc rgb '#00ced1' pi 5 pt 5 ps 1 lt 1 lw 2 # --- darkturquoise
+set style line 6 lc rgb '#00ced1' pi 6 pt 5 ps 1 lt 1 lw 2 # --- darkturquoise
 set style line 7 lc rgb '#ff00ff' pi 5 pt 7 ps 1 lt 1 lw 2 # --- magenta
 set style line 8 lc rgb '#87ceeb' pi 5 pt 8 ps 1 lt 1 lw 2 # --- skyblue
 set key top left
@@ -19,13 +19,22 @@ set xlabel 'N'
 set ylabel 'Time / N'
 set xrange [1:10]
 set yrange [0:*]
-unset xtics
-unset ytics
+buffer_size = 8
 epsilon(fanout, B) = log(fanout)/log(B)
-f(x,B) = (1/(epsilon(2,B)*(B**(1-epsilon(2,B))))) * (log(x)/log(B))
+brodal(x,B) = (1/(epsilon(2,B)*(B**(1-epsilon(2,B))))) * (log(x)/log(B))
+internal(x) = log(x)/log(2)
+arge(x,B) = log(x)/log(B)
+rtree(x) = 0.8*x
+rstar(x) = x
+mysql(x) = x
+unset ytics
+unset xtics
 
-plot f(x,1024*1024/8) with linespoint title sprintf("buffer1MB") ls 2,\
-     f(x,1024*1024/4) with linespoint title sprintf("buffer2MB") ls 3,\
-     f(x,1024*1024/2) with linespoint title sprintf("buffer4MB") ls 4,\
-     f(x,1024*1024) with linespoint title sprintf("buffer8MB") ls 5
+plot brodal(x,buffer_size) with linespoint title sprintf("Brodal") ls 2,\
+     internal(x) with linespoint title sprintf("McCreight Internal PST") ls 3,\
+     arge(x,buffer_size) with linespoint title sprintf("Arge et al") ls 4,\
+     rtree(x) with linespoint title sprintf("RTree") ls 5,\
+     rstar(x) with linespoint title sprintf("R*Tree") ls 6,\
+     mysql(x) with linespoint title sprintf("MySQL") ls 7
+
 
---
+++ plots/query_complexities/plot_query_complexity.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/gnuplot
+set terminal postscript eps enhanced color font 'Verdana,11'
+set output 'query_complexity.eps'
+set style line 11 lc rgb '#808080' lt 1
+set border 3 back ls 11
+set tics nomirror
+set style line 12 lc rgb '#808080' lt 0 lw 1
+set grid back ls 12
+set style line 1 lc rgb '#8b1a0e' pi 5 pt 1 ps 1 lt 1 lw 2 # --- red
+set style line 2 lc rgb '#5e9c36' pi 6 pt 6 ps 1 lt 1 lw 2 # --- green
+set style line 3 lc rgb '#88419d' pi 5 pt 2 ps 1 lt 1 lw 2 # --- purple
+set style line 4 lc rgb '#225ea8' pi 5 pt 3 ps 1 lt 1 lw 2 # --- blue
+set style line 5 lc rgb '#000000' pi 5 pt 4 ps 1 lt 1 lw 2 # --- black
+set style line 6 lc rgb '#00ced1' pi 6 pt 5 ps 1 lt 1 lw 2 # --- darkturquoise
+set style line 7 lc rgb '#ff00ff' pi 5 pt 7 ps 1 lt 1 lw 2 # --- magenta
+set style line 8 lc rgb '#87ceeb' pi 7 pt 8 ps 1 lt 1 lw 2 # --- skyblue
+set key top left
+set xlabel 'N'
+set ylabel 'Time / N'
+set xrange [1:10]
+set yrange [0:*]
+buffer_size = 32
+epsilon(fanout, B) = log(fanout)/log(B)
+#brodal(x,B) = (1/epsilon(2,B)) * (log(x)/log(B))
+brodal(x,B,fanout) = (1/epsilon(fanout,B)) * (log(x)/log(B))
+internal(x) = log(x)/log(2)
+arge(x,B) = log(x)/log(B)
+rtree(x) = 0.8*x
+rstar(x) = x
+mysql(x) = x
+unset ytics
+unset xtics
+
+plot brodal(x,buffer_size,2) with linespoint title sprintf("Brodal with fanout 2") ls 2,\
+     brodal(x,buffer_size,16) with linespoint title sprintf("Brodal with fanout 16") ls 8,\
+     internal(x) with linespoint title sprintf("McCreight Internal PST") ls 3,\
+     arge(x,buffer_size) with linespoint title sprintf("Arge et al") ls 4,\
+     rtree(x) with linespoint title sprintf("RTree") ls 5,\
+     rstar(x) with linespoint title sprintf("R*Tree") ls 6,\
+     mysql(x) with linespoint title sprintf("MySQL") ls 7
+
+
