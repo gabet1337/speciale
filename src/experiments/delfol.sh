32ed1b62e119a94627118ec06a0e56e710685218
Christoffer <chh@nextmedia.dk>
2016-04-28 09:59:04 +0200

    Deletes all folders of substructures

---
+++ src/experiments/delfol.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+for i in 1* ; do
+    rm -rf $i
+done
+
+for i in 2* ; do
+    rm -rf $i
+done
+
+for i in 3* ; do
+    rm -rf $i
+done
+
+for i in 4* ; do
+    rm -rf $i
+done
+
+for i in 5* ; do
+    rm -rf $i
+done
+
+for i in 6* ; do
+    rm -rf $i
+done
+
+for i in 7* ; do
+    rm -rf $i
+done
+
+for i in 8* ; do
+    rm -rf $i
+done
+
+for i in 9* ; do
+    rm -rf $i
+done
+
+for i in c_* ; do
+    rm -rf $i
+done
