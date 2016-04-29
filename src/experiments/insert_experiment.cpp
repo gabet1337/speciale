ec9a924733bb5ed8640d3b576d69f8f0ba22680b
Peter Gabrielsen <peterg@cs.au.dk>
2016-04-29 15:54:26 +0200

    unsigned long long data size

--- src/experiments/insert_experiment.cpp
+++ src/experiments/insert_experiment.cpp
@@ -8,13 +8,13 @@ class insert_experiment : public base_experiment {
   ~insert_experiment() {}
 
   void run_experiment(run_instance instance) {
-
+    typedef unsigned long long ull;
     stream data = get_data_stream("../data/insert_experiment");
     auto pst = PST_factory(instance.type, instance.buffer_size, instance.epsilon);
-    size_t data_read = 0;
-    size_t data_size = sizeof(point);
-    size_t interval = 10 * 1024 * 1024; //10 mb intervals
-    size_t print_interval = 1024 * 1024;
+    ull data_read = 0;
+    ull data_size = sizeof(point);
+    ull interval = 10 * 1024 * 1024; //10 mb intervals
+    ull print_interval = 1024 * 1024;
     test::clock exp_time;
     exp_time.start();
     long long one_day = 60*60*24;
@@ -46,9 +46,9 @@ class insert_experiment : public base_experiment {
 int main() {
 
   insert_experiment ie("insert_experiment");
+  ie.add(5, "MySQL", common::PST_VARIANT::MYSQL, MYSQL_BUFFER_SIZE, MYSQL_EPSILON);
   ie.add(3, "Internal", common::PST_VARIANT::INTERNAL, INTERNAL_BUFFER_SIZE, INTERNAL_EPSILON);
   ie.add(4, "Boost R-tree", common::PST_VARIANT::RTREE, RTREE_BUFFER_SIZE, RTREE_EPSILON);
-  ie.add(5, "MySQL", common::PST_VARIANT::MYSQL, MYSQL_BUFFER_SIZE, MYSQL_EPSILON);
   ie.add(1, "Gerth", common::PST_VARIANT::GERTH, GERTH_BUFFER_SIZE, GERTH_EPSILON);
   ie.add(2, "Arge", common::PST_VARIANT::ARGE, ARGE_BUFFER_SIZE, ARGE_EPSILON);
   ie.add(6, "libspatial", common::PST_VARIANT::SPATIAL, SPATIAL_BUFFER_SIZE, SPATIAL_EPSILON);
