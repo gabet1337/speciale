aa1192c110b03c0c1422babe1ee546b5cba51a33
Peter Gabrielsen <peterg@cs.au.dk>
2016-05-25 16:08:20 +0200

    query complexity fixed

--- plots/query_complexities/plot_query_complexity.sh
+++ plots/query_complexities/plot_query_complexity.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/gnuplot
-set terminal postscript eps enhanced color font 'Verdana,11'
+set terminal postscript eps enhanced color font 'Verdana,12'
 set output 'query_complexity.eps'
-set style line 11 lc rgb '#808080' lt 1
+set style line 11 lc rgb '#000000' lt 1
 set border 3 back ls 11
 set tics nomirror
-set style line 12 lc rgb '#808080' lt 0 lw 1
+set style line 12 lc rgb '#000000' lt 0 lw 1
 set grid back ls 12
 set style line 1 lc rgb '#8b1a0e' pi 5 pt 1 ps 1 lt 1 lw 2 # --- red
 set style line 2 lc rgb '#5e9c36' pi 6 pt 6 ps 1 lt 1 lw 2 # --- green
 set style line 3 lc rgb '#88419d' pi 5 pt 2 ps 1 lt 1 lw 2 # --- purple
-set style line 4 lc rgb '#225ea8' pi 5 pt 3 ps 1 lt 1 lw 2 # --- blue
+set style line 4 lc rgb '#225ea8' pi 6 pt 3 ps 1 lt 1 lw 2 # --- blue
 set style line 5 lc rgb '#000000' pi 5 pt 4 ps 1 lt 1 lw 2 # --- black
 set style line 6 lc rgb '#00ced1' pi 6 pt 5 ps 1 lt 1 lw 2 # --- darkturquoise
 set style line 7 lc rgb '#ff00ff' pi 5 pt 7 ps 1 lt 1 lw 2 # --- magenta
@@ -27,16 +27,16 @@ internal(x) = log(x)/log(2)
 arge(x,B) = log(x)/log(B)
 rtree(x) = 0.8*x
 rstar(x) = x
-mysql(x) = x
+mysql(x) = log(x)/log(buffer_size)+0.05
 unset ytics
 unset xtics
 
 plot brodal(x,buffer_size,2) with linespoint title sprintf("Brodal with fanout 2") ls 2,\
      brodal(x,buffer_size,16) with linespoint title sprintf("Brodal with fanout 16") ls 8,\
-     internal(x) with linespoint title sprintf("McCreight Internal PST") ls 3,\
-     arge(x,buffer_size) with linespoint title sprintf("Arge et al") ls 4,\
-     rtree(x) with linespoint title sprintf("RTree") ls 5,\
-     rstar(x) with linespoint title sprintf("R*Tree") ls 6,\
-     mysql(x) with linespoint title sprintf("MySQL") ls 7
+     arge(x,buffer_size) with linespoint title sprintf("Arge") ls 1,\
+     internal(x) with linespoint title sprintf("Internal PST") ls 5,\
+     rtree(x) with linespoint title sprintf("Boost RTree") ls 3,\
+     rstar(x) with linespoint title sprintf("libspatial R*Tree") ls 6,\
+     mysql(x) with linespoint title sprintf("MySQL") ls 4
 
 
