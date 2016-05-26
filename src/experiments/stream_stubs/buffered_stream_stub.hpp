05e90f2a31fa74ad76b6282b3cc854fbad9c34eb
Peter Gabrielsen <peterg@cs.au.dk>
2016-05-26 12:25:44 +0200

    bostream

--- src/experiments/stream_stubs/buffered_stream_stub.hpp
+++ src/experiments/stream_stubs/buffered_stream_stub.hpp
@@ -18,7 +18,8 @@ namespace experiment {
     void print();
   private:
     io::buffered_stream<int> *input_stream;
-    io::buffered_stream<int> *output_stream;
+    //io::buffered_stream<int> *output_stream;
+    io::bostream *output_stream;
   };
 
   buffered_stream_stub::buffered_stream_stub() {
@@ -26,21 +27,22 @@ namespace experiment {
 
   buffered_stream_stub::buffered_stream_stub(size_t buffer_size, double epsilon) {
     input_stream = new io::buffered_stream<int>(buffer_size);
-    output_stream = new io::buffered_stream<int>(buffer_size);
+    //output_stream = new io::buffered_stream<int>(buffer_size);
+    output_stream = new io::bostream(buffer_size);
     input_stream->open("../data/insert_experiment");
-    output_stream->open("../data/temp_buffered_out");
+    output_stream->create_stream("../data/temp_buffered_out");
   }
 
   buffered_stream_stub::~buffered_stream_stub() {
     input_stream->close();
-    output_stream->close();
+    output_stream->close_stream();
     delete input_stream;
     delete output_stream;
     util::remove_file("../data/temp_buffered_out");
   }
 
   void buffered_stream_stub::insert(const point &p) {
-    output_stream->write(1337);
+    output_stream->write_stream(1337);
   }
 
   void buffered_stream_stub::remove(const point &p) {
