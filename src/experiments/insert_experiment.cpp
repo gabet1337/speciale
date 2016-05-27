e5b7d11bf1620770793b4dad2f97d42fc154dc8a
Peter Gabrielsen <peterg@cs.au.dk>
2016-05-27 13:50:26 +0200

    run this on C computer

--- src/experiments/insert_experiment.cpp
+++ src/experiments/insert_experiment.cpp
@@ -13,8 +13,8 @@ class insert_experiment : public base_experiment {
     auto pst = PST_factory(instance.type, instance.buffer_size, instance.epsilon);
     ull data_read = 0;
     ull data_size = sizeof(point);
-    ull interval = 10 * 1024 * 1024; //10 mb intervals
-    ull print_interval = 1024 * 1024;
+    ull interval = 1 * 1024 * 1024; //10 mb intervals
+    ull print_interval = 1024 * 64;
     test::clock exp_time;
     exp_time.start();
     long long one_day = 60*60*24;
@@ -23,7 +23,7 @@ class insert_experiment : public base_experiment {
       point p = data.read();
       pst->insert(p);
       data_read += data_size;
-      if (exp_time.elapsed() > one_day) {
+      if (data_read > 2ULL*1024ULL*1024ULL*1024ULL) {
         std::cout << "TIMES UP!" << std::endl;
         measure_everything(instance.id, data_read/(1024*1024));
         save_results(instance);
@@ -35,7 +35,7 @@ class insert_experiment : public base_experiment {
       }
 
       if (data_read % print_interval == 0) {
-        std::cout << data_read/(1024*1024) << "MB" << std::endl;
+        std::cout << data_read/1024 << "Kb" << std::endl;
       }
     }
     delete pst;
@@ -46,11 +46,11 @@ class insert_experiment : public base_experiment {
 int main() {
   
   insert_experiment ie("insert_experiment");
-  ie.add(1, "Gerth", common::PST_VARIANT::GERTH, GERTH_BUFFER_SIZE, GERTH_EPSILON);
-  ie.add(2, "Arge", common::PST_VARIANT::ARGE, ARGE_BUFFER_SIZE, ARGE_EPSILON);
-  ie.add(3, "Internal", common::PST_VARIANT::INTERNAL, INTERNAL_BUFFER_SIZE, INTERNAL_EPSILON);
-  ie.add(4, "Boost R-tree", common::PST_VARIANT::RTREE, RTREE_BUFFER_SIZE, RTREE_EPSILON);
-  ie.add(6, "libspatial", common::PST_VARIANT::SPATIAL, SPATIAL_BUFFER_SIZE, SPATIAL_EPSILON);
+  // ie.add(1, "Gerth", common::PST_VARIANT::GERTH, ARGE_BUFFER_SIZE, log(2.5)/log(ARGE_BUFFER_SIZE));
+  //ie.add(2, "Arge", common::PST_VARIANT::ARGE, ARGE_BUFFER_SIZE, ARGE_EPSILON);
+  //ie.add(3, "Internal", common::PST_VARIANT::INTERNAL, INTERNAL_BUFFER_SIZE, INTERNAL_EPSILON);
+  //ie.add(4, "Boost R-tree", common::PST_VARIANT::RTREE, RTREE_BUFFER_SIZE, RTREE_EPSILON);
+  //ie.add(6, "libspatial", common::PST_VARIANT::SPATIAL, SPATIAL_BUFFER_SIZE, SPATIAL_EPSILON);
   ie.add(5, "MySQL", common::PST_VARIANT::MYSQL, MYSQL_BUFFER_SIZE, MYSQL_EPSILON);
   ie.run();
   ie.plot();
