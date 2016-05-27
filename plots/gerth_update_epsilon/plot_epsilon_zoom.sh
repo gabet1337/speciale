5b3cbaf4320ab2f0692a4d02a05c77a5a083c3ef
Christoffer <chh@nextmedia.dk>
2016-05-27 13:52:52 +0200

    better figure

--- plots/gerth_update_epsilon/plot_epsilon_zoom.sh
+++ plots/gerth_update_epsilon/plot_epsilon_zoom.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/gnuplot
-set terminal postscript eps enhanced color font 'Verdana,20'
+set terminal postscript eps enhanced color font 'Verdana,24'
 set output 'gerth_epsilon_zoom.eps'
 set style line 11 lc rgb '#000000' lt 1
 set border 3 back ls 11
