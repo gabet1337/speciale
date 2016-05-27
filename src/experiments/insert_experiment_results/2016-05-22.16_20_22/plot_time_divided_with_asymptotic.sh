bc3b42463ad2abfa1442355a73fe1dcb7c644456
Peter Gabrielsen <peterg@cs.au.dk>
2016-05-27 14:17:30 +0200

    gerth results

--- src/experiments/insert_experiment_results/2016-05-22.16_20_22/plot_time_divided_with_asymptotic.sh
+++ src/experiments/insert_experiment_results/2016-05-22.16_20_22/plot_time_divided_with_asymptotic.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/gnuplot
-set terminal postscript eps enhanced color font 'Verdana,12'
+set terminal postscript eps enhanced color font 'Verdana,24'
 
 set style line 11 lc rgb '#000000' lt 1
-set border 3 back ls 11
+set border 3+8 back ls 11
 set tics nomirror
 set style line 12 lc rgb '#000000' lt 0 lw 1
-#set grid back ls 12
+set grid back ls 12
 set style line 1 lc rgb '#8b1a0e' pt 1 ps 1 lt 1 lw 2 # --- red
 set style line 2 lc rgb '#5e9c36' pi 8 pt 6 ps 1 lt 1 lw 2 # --- green
-set style line 3 lc rgb '#88419d' pi 6 pt 2 ps 1 lt 1 lw 2 # --- purple
-set style line 4 lc rgb '#225ea8' pi 8 pt 3 ps 1 lt 1 lw 2 # --- blue
-set style line 5 lc rgb '#000000' pi 10 pt 4 ps 1 lt 1 lw 2 # --- black
+set style line 3 lc rgb '#88419d' pi 20 pt 2 ps 1 lt 1 lw 5 # --- purple
+set style line 4 lc rgb '#225ea8' pi 10 pt 3 ps 1 lt 1 lw 2 # --- blue
+set style line 5 lc rgb '#000000' pi 20 pt 4 ps 1 lt 1 lw 5 # --- black
 set style line 6 lc rgb '#00ced1' pt 5 ps 1 lt 1 lw 2 # --- darkturquoise
 set style line 7 lc rgb '#ff00ff' pt 7 ps 1 lt 1 lw 2 # --- magenta
 set style line 8 lc rgb '#87ceeb' pt 8 ps 1 lt 1 lw 2 # --- skyblue
-set key top right
+set key top left
+set xtics rotate by 45 offset -0.9,-1.3
 set xlabel 'N (input size in Mb)'
-#set ylabel 'Time (s) / N (input size in Mb)'
+set ylabel 'Time (s) per insert / N'
+set y2label 'I/Os per insert / N'
+set y2tics
 #set xrange [0:3500]
-#set yrange [0:4000]
-unset ytics
+set yrange [0:1]
+set y2range [0:2000]
+#unset ytics
+set autoscale y2
 B = 1024*1024
 epsilon(fanout) = log(fanout)/log(B)
 f(x,eps) = (1/(eps*B**(1-eps))) * (log(x)/log(B))
-set output 'insert_experiment_results/2016-05-22.16_20_22/time_divided_with_asymptotic_brodal.eps'
-plot 'insert_experiment_results/2016-05-22.16_20_22/gerth_Gerth' u 1:($2/(($1*1024*1024)*f(($1*1024*1024),epsilon(2)))) notitle '' w lp ls 5
-set output 'insert_experiment_results/2016-05-22.16_20_22/ios_divided_with_asymptotic_brodal.eps'
-plot 'insert_experiment_results/2016-05-22.16_20_22/gerth_Gerth' u 1:($3/(($1*1024*1024)*f(($1*1024*1024),epsilon(2)))) notitle w lp ls 5
+set output 'insert_experiment_results/2016-05-22.16_20_22/time_and_ios.eps'
+plot 'insert_experiment_results/2016-05-22.16_20_22/gerth_Gerth' u 1:($2/(($1*1024*1024)*f(($1*1024*1024),epsilon(2)))) title 'time' w lp ls 5 axes x1y1,\
+     'insert_experiment_results/2016-05-22.16_20_22/gerth_Gerth' u 1:($3/(($1*1024*1024)*f(($1*1024*1024),epsilon(2)))) title 'I/Os' w lp ls 3 axes x2y2
