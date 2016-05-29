e13518e74ab9ad9530a5a74c6979ad73843aee13
Peter Gabrielsen <peterg@cs.au.dk>
2016-05-29 16:27:34 +0200

    font size 14 instead

--- plots/scaling_discrepancy_hdd_cpu/plot_discrepancy.sh
+++ plots/scaling_discrepancy_hdd_cpu/plot_discrepancy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/gnuplot
-set terminal postscript eps enhanced color font 'Verdana,12'
+set terminal postscript eps enhanced color font 'Verdana,14'
 set output 'scaling_discrepancy_hdd_cpu.eps'
 set style line 11 lc rgb '#000000' lt 1
 set border 3 back ls 11
