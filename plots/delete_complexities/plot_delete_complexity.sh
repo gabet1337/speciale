44883a60f7c8a0dde218d67ef8fb6b088c40b7c0
Christoffer <chh@nextmedia.dk>
2016-06-05 18:32:39 +0200

    Better delete complexity plot

--- plots/delete_complexities/plot_delete_complexity.sh
+++ plots/delete_complexities/plot_delete_complexity.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/gnuplot
-set terminal postscript eps enhanced color font 'Verdana,12'
+set terminal postscript eps enhanced color size 7.0,2.67 font 'Verdana,16'
 set output 'delete_complexity.eps'
 set style line 11 lc rgb '#000000' lt 1
 set border 3 back ls 11
 set tics nomirror
 set style line 12 lc rgb '#000000' lt 0 lw 1
 set grid back ls 12
-set style line 1 lc rgb '#8b1a0e' pi 3 pt 1 ps 1 lt 1 lw 2 # --- red
-set style line 2 lc rgb '#5e9c36' pi 5 pt 6 ps 1 lt 1 lw 2 # --- green
-set style line 3 lc rgb '#88419d' pi 5 pt 2 ps 1 lt 1 lw 2 # --- purple
-set style line 4 lc rgb '#225ea8' pi 5 pt 3 ps 1 lt 1 lw 2 # --- blue
-set style line 5 lc rgb '#000000' pi 5 pt 4 ps 1 lt 1 lw 2 # --- black
-set style line 6 lc rgb '#00ced1' pi 6 pt 5 ps 1 lt 1 lw 2 # --- darkturquoise
-set style line 7 lc rgb '#ff00ff' pi 7 pt 7 ps 1 lt 1 lw 2 # --- magenta
-set style line 8 lc rgb '#87ceeb' pi 5 pt 8 ps 1 lt 1 lw 2 # --- skyblue
+set style line 1 lc rgb '#8b1a0e' pi 3 pt 1 ps 1.4 lt 1 lw 2 # --- red
+set style line 2 lc rgb '#5e9c36' pi 5 pt 6 ps 1.4 lt 1 lw 2 # --- green
+set style line 3 lc rgb '#88419d' pi 5 pt 2 ps 1.4 lt 1 lw 2 # --- purple
+set style line 4 lc rgb '#225ea8' pi 5 pt 3 ps 1.4 lt 1 lw 2 # --- blue
+set style line 5 lc rgb '#000000' pi 5 pt 4 ps 1.4 lt 1 lw 2 # --- black
+set style line 6 lc rgb '#00ced1' pi 6 pt 5 ps 1.4 lt 1 lw 2 # --- darkturquoise
+set style line 7 lc rgb '#ff00ff' pi 7 pt 7 ps 1.4 lt 1 lw 2 # --- magenta
+set style line 8 lc rgb '#87ceeb' pi 5 pt 8 ps 1.4 lt 1 lw 2 # --- skyblue
 set key top left
 set xlabel 'N'
 set ylabel 'Time / N'
