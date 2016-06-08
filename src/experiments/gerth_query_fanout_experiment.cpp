9f53b08a5501217241dabed261c81c6c4cd028b5
Christoffer <chh@nextmedia.dk>
2016-06-08 16:08:49 +0200

    Gerth query fanout experiment

--- src/experiments/gerth_query_fanout_experiment.cpp
+++ src/experiments/gerth_query_fanout_experiment.cpp
@@ -10,13 +10,13 @@ class gerth_query_fanout_experiment : public base_experiment {
 
   void run_experiment(run_instance instance) {
     typedef unsigned long long ull;
-    stream data = get_data_stream("../data/insert_experiment");
+    stream data = get_data_stream("../data/query_experiment");
     auto pst = PST_factory(instance.type, instance.buffer_size, instance.epsilon);
     ull data_read = 0;
     ull data_size = sizeof(point);
     ull interval = 25 * 1024 * 1024; // 25 MB intervals
     ull print_interval = 1024 * 1024;
-    ull stop_at = 1024ULL * 1024ULL * 1024ULL * 1ULL; // 1 GB
+    ull stop_at = 512ULL * 1024ULL * 1024ULL * 1ULL; // 1 GB
 
     restart_timers();
 
@@ -39,7 +39,7 @@ class gerth_query_fanout_experiment : public base_experiment {
 	util::remove_directory("query_result4");
 	util::remove_directory("query_result5");
         restart_timers();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
           pst->report(107374182, 214748364, 0, "query_result1");
           pst->report(472446402, 837518622, 429496729, "query_result2");
           pst->report(1009317314, 1245540515, 1288490188, "query_result3");
@@ -71,8 +71,8 @@ int main() {
 
   gerth_query_fanout_experiment gfe("gerth_query_fanout_experiment");
   //gfe.add(1, "fanout2", common::PST_VARIANT::GERTH, 1024*1024, log(2.5)/log(1024.0*1024.0));
-  gfe.add(2, "fanout4", common::PST_VARIANT::GERTH, 1024*1024, log(4.5)/log(1024.0*1024.0));
-  //gfe.add(3, "fanout8", common::PST_VARIANT::GERTH, 1024*1024, log(8.5)/log(1024.0*1024.0));
+  //gfe.add(2, "fanout4", common::PST_VARIANT::GERTH, 1024*1024, log(4.5)/log(1024.0*1024.0));
+  gfe.add(3, "fanout16", common::PST_VARIANT::GERTH, 1024*1024/16, log(16.5)/log(1024.0*1024.0/16.0));
   //gfe.add(4, "fanout16", common::PST_VARIANT::GERTH, 1024*1024, log(16.5)/log(1024.0*1024.0));
   //gfe.add(5, "fanout32", common::PST_VARIANT::GERTH, 1024*1024, log(32.5)/log(1024.0*1024.0));
   gfe.run();
