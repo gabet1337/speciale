4a31df385f13c807bbf36037bfe8ae430f962199
Peter Gabrielsen <peterg@cs.au.dk>
2016-06-18 14:40:49 +0200

    fixed arge experiment

--- src/experiments/insert_experiment_results/2016-05-25.10_23_00/plot_time_divided_with_asymptotic.sh
+++ src/experiments/insert_experiment_results/2016-05-25.10_23_00/plot_time_divided_with_asymptotic.sh
@@ -17,19 +17,19 @@ set style line 8 lc rgb '#87ceeb' pt 8 ps 1 lt 1 lw 2 # --- skyblue
 set key top left
 set xtics rotate by -35 offset -0.3,0
 set xlabel 'N (input size in Mb)'
-set ylabel 'Time (s) per insert / N'
-set y2label 'I/Os per insert / N'
+set ylabel 'Time (s) per insert / log_B N'
+set y2label 'I/Os per insert / log_B N'
 set y2tics
 #set xrange [0:3500]
-set yrange [0:2]
-set y2range [0:2000]
+set y2range [0:2]
+set yrange [0:2000]
 #unset ytics
-set autoscale y2
+set autoscale y
 B = 1024*1024
 epsilon(fanout) = log(fanout)/log(B)
 f(x,eps) = (1/(eps*B**(1-eps))) * (log(x)/log(B))
 
 set output 'insert_experiment_results/2016-05-25.10_23_00/time_and_ios.eps'
-plot 'insert_experiment_results/2016-05-25.10_23_00/arge_Arge' u 1:($3/(($1*1024*1024)*(log($1*1024*1024)/log(4*1024)))) title "time" w lp ls 5 axes x1y1,\
-     'insert_experiment_results/2016-05-25.10_23_00/arge_Arge' u 1:($2/(($1*1024*1024)*(log($1*1024*1024)/log(4*1024)))) title "I/Os" w lp ls 7 axes x1y2
+plot 'insert_experiment_results/2016-05-25.10_23_00/arge_Arge' u 1:($2/(($1*1024*1024)*(log($1*1024*1024)/log(4*1024)))) title "time" w lp ls 5 axes x1y1,\
+     'insert_experiment_results/2016-05-25.10_23_00/arge_Arge' u 1:($3/(($1*1024*1024)*(log($1*1024*1024)/log(4*1024)))) title "I/Os" w lp ls 7 axes x1y2
 
