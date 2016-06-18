f93dc18b0483f6fdbe61b0012fe5b944ece4f50e
Christoffer <chh@nextmedia.dk>
2016-06-18 17:10:45 +0200

    Fixes wrong plot

--- src/experiments/insert_experiment_results/2016-05-22.16_20_22/plot_time_divided_with_asymptotic.sh
+++ src/experiments/insert_experiment_results/2016-05-22.16_20_22/plot_time_divided_with_asymptotic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/gnuplot
 set terminal postscript eps enhanced color font 'Verdana,24'
-
+#set ytics font "Verdana,12"
 set style line 11 lc rgb '#000000' lt 1
 set border 3+8 back ls 11
 set tics nomirror
@@ -17,8 +17,8 @@ set style line 8 lc rgb '#87ceeb' pi 20 pt 8 ps 1 lt 1 lw 5 # --- skyblue
 set key top left
 set xtics rotate by -35 offset -0.2 -1.5
 set xlabel 'N (input size in Mb)'
-set ylabel 'Time (s) per insert / N'
-set y2label 'I/Os per insert / N'
+set ylabel 'Time (s) per insert / 1/({/Symbol \145} B^{1-{/Symbol \145}}) log_{B} N)' font "Verdana,18"
+set y2label 'I/Os per insert / 1/({/Symbol \145} B^{1-{/Symbol \145}}) log_{B} N)' font "Verdana,18"
 set y2tics
 #set xrange [0:3500]
 set yrange [0:1]
@@ -30,4 +30,4 @@ epsilon(fanout) = log(fanout)/log(B)
 f(x,eps) = (1/(eps*B**(1-eps))) * (log(x)/log(B))
 set output 'insert_experiment_results/2016-05-22.16_20_22/time_and_ios.eps'
 plot 'insert_experiment_results/2016-05-22.16_20_22/gerth_Gerth' u 1:($2/(($1*1024*1024)*f(($1*1024*1024),epsilon(2)))) title 'time' w lp ls 5 axes x1y1,\
-     'insert_experiment_results/2016-05-22.16_20_22/gerth_Gerth' u 1:($3/(($1*1024*1024)*f(($1*1024*1024),epsilon(2)))) title 'I/Os' w lp ls 7 axes x1y2
+     'insert_experiment_results/2016-05-22.16_20_22/gerth_Gerth' u 1:(($3)/(($1*1024*1024)*f(($1*1024*1024),epsilon(2)))) title 'I/Os' w lp ls 7 axes x1y2
